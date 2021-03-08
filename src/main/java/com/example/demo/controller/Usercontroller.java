90293c03318639429507d4c5bdef4dc3d060f2e4
YXalix <516875454@qq.com>
2021-03-08 16:31:16 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/Usercontroller.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.UserRepository;
+import com.example.demo.domain.User;
+
+@RestController
+@RequestMapping("user")
+public class Usercontroller {
+
+   @Autowired
+   private UserRepository userRepository;
+
+   @RequestMapping("/getAllUser")
+   @ResponseBody
+   public List<User> findAll() {
+       List<User> list = new ArrayList<User>();
+       list = userRepository.findAll();
+       return list;
+   }
+
+   @RequestMapping("/getByUserName")
+   @ResponseBody
+   public User getByUserName(String userName) {
+       User user = userRepository.findByUserName(userName);
+       return user;
+   }
+
+}
